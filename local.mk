c5e30e22a1570f6102cabdfb5068fc502ca63989
buddha314 <buddha_34@yahoo.com>
2018-06-07 13:25:05 -0700

    adding local.mk for now

---
+++ local.mk
@@ -0,0 +1,10 @@
+CHINGON_HOME=$(HOME)/git/chingon
+NUMSUCH_HOME=$(HOME)/git/numsuch
+CDO_HOME=$(HOME)/git/cdo
+CDOEXTRAS_HOME=$(HOME)/git/cdo-extras
+CHARCOAL_HOME=$(HOME)/git/Charcoal
+BLAS_HOME=/usr/include/
+CHREST_HOME=$(HOME)/git/chrest
+RELCH_HOME=$(HOME)/git/Relch
+EPOCH_HOME=$(HOME)/git/epoch
+MOSCHITTO_HOME=$(HOME)/git/moschitto
